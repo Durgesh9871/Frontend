195f21071839876871a2772bab57b3c83b5a129f
Durgesh9871 <devisingh9871686098@gmail.com>
2023-05-12 08:04:02 +0530

    file

--- my-app/src/Pages/User.jsx
+++ my-app/src/Pages/User.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {Box, SimpleGrid} from "@chakra-ui/react"
 import { FilterBox } from '../UserPageComponent/FilterBox'
 import { DisplayData } from '../UserPageComponent/DisplayData'
+import { useDispatch, useSelector } from 'react-redux'
+import { getData } from '../Redux/action'
 // import "./user.css"
 
 const User = () => {
+      
+
+  const dispatch = useDispatch()
+//  getting data and display it ------------
+  const {isLoading , userData , isError} = useSelector((state)=>{
+    return {
+      isLoading:state.isLoading ,
+      userData: state.userData,
+      isError: state.isError , 
+
+    }
+  })
+
+  console.log(userData ,"snb")
+
+  useEffect(()=>{
+    dispatch(getData)
+  },[])
+    
+
   return (
     <Box id='filterMainBox_&_DisplayData' style={{border:"1px  red" ,width:"92%" , margin:"auto" , height:"auto" , display:"flex" , justifyContent:"space-between" }}>
       {/*  Filter main box --- */}
@@ -21,12 +43,8 @@ const User = () => {
 {/* Data display in format of grid */}
       <SimpleGrid columns={{base:1, sm: 1, md: 2, lg: 3,xl: 3,'2xl': 3,}} spacingY={10}  >
      
-      <DisplayData />
-       <DisplayData />
-       <DisplayData />
-       <DisplayData />
-       <DisplayData />
-       <DisplayData />
+    {  <DisplayData />}
+     
 
       </SimpleGrid>
       
