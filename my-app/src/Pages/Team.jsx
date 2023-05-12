678351334ebb3c17fb0dece8c53bb7c4a682f203
Durgesh9871 <devisingh9871686098@gmail.com>
2023-05-12 13:33:57 +0530

    file

--- my-app/src/Pages/Team.jsx
+++ my-app/src/Pages/Team.jsx
@@ -5,12 +5,20 @@ import { Box, Heading } from '@chakra-ui/react'
 
 
 const Team = () => {
+    const userTeam = JSON.parse(localStorage.getItem("userTeam")) || []
+
   return (
     <Box >
      <Heading color="grey" textAlign="center" m="20px 0px"><u>Team Members</u></Heading>
 
      {/* teams box */}
-    <TeamBox />
+      {userTeam?.length > 0 && userTeam.map((ele , i)=>{
+         return  <TeamBox key={ele.id} item={ele} /> 
+      })
+        
+      }
+
+   
 
     </Box>
   )
--- my-app/src/TeamComponent/TeamBox.jsx
+++ my-app/src/TeamComponent/TeamBox.jsx
@@ -3,28 +3,28 @@ import { Box, Heading , Image , Text} from '@chakra-ui/react'
 import {AiFillDelete} from "react-icons/ai"
 
 
-const TeamBox = () => {
+const TeamBox = ({item}) => {
   return (
-    <Box shadow="base" display="flex" justifyContent="space-around" p="12px" alignItems="center" className='hoverProductDiv' w={{base:"auto", sm: "55vw", md: "40vw", lg: "28vw" ,xl: "23vw",'2xl': "23vw",}} border="2px  red" >
+    <Box shadow="base" display="flex" m="40px auto" justifyContent="space-around" p="12px" alignItems="center" className='hoverProductDiv' w={{base:"auto", sm: "55vw", md: "40vw", lg: "28vw" ,xl: "23vw",'2xl': "23vw",}}  >
       
       {/*  image box */}
       <Box className='imageTeam'>
-      <Image src='https://bit.ly/dan-abramov' alt='Dan Abramov' width="100px" borderRadius="50%" />
+      <Image src={item.avatar} alt={item.first_name} width="100px" background="black" border="2px solid #727272" borderRadius="50%" />
       </Box>
 
       {/*  Detail Box */}
-      <Box>
-      <Heading fontSize="16px" fontWeight="700" color="#303030" textAlign="left"> Domain: <span style={{color:"grey"}}>{"IT Sector"}</span></Heading>
+      <Box width="60%">
+      <Heading fontSize="16px" fontWeight="700" color="#303030" textAlign="left"> Domain: <span style={{color:"grey"}}>{item.domain}</span></Heading>
     
      
-      <Heading fontSize="15.5px" fontWeight="600" color="#727272" textAlign="left">{"durgesh" + " " + "sig"}</Heading> 
+      <Heading fontSize="15.5px" className='control' fontWeight="600" color="#727272" textAlign="left">{item.first_name + " " + item.last_name}</Heading> 
             
-         <Text fontSize='14px'  fontWeight="500" color="#727272" textAlign="left"  >{"male"}</Text> 
-         <Text fontSize='14px' className='control'  fontWeight="500" color="#727272" textAlign="left"  >{"durgesh@gmail.com"}</Text> 
+         <Text fontSize='14px' className='control'  fontWeight="500" color="#727272" textAlign="left"  >{item.gender}</Text> 
+         <Text fontSize='14px' className='control'  fontWeight="500" color="#727272" textAlign="left"  >{item.email}</Text> 
 
         {/*  delete button inside div */}
         <Box display="flex" justifyContent="space-between" mt="10px">
-         <Text fontSize='14px'  fontWeight="600" color={true == true ? "#00f700" : "#e34f4f"} textAlign="left"  >{true == true ? "Available" : "Not Available"}</Text> 
+         <Text fontSize='14px'  fontWeight="600" color={item.available == true ? "#00f700" : "#e34f4f"} textAlign="left"  >{item.available == true ? "Available" : "Not Available"}</Text> 
         
         <AiFillDelete style={{fontSize:"20px" , color:"red" , cursor:"pointer"}} />
 
